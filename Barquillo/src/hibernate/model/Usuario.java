aa715211b320c3792139a35bdeab4d31c2f64186
INDETP <sellteje@gmail.com>
2017-06-13 13:09:28 +0200

    encriptado pass

--- Barquillo/src/hibernate/model/Usuario.java
+++ Barquillo/src/hibernate/model/Usuario.java
@@ -1,11 +1,5 @@
 package hibernate.model;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-
 public class Usuario {
 	
 	private int referencia;
